c5836d918f9c03bb8d9ade4cd8166bce420cdd31
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-09-27 14:37:57 +0200

    initial commit

---
+++ ADMIN PANNEL/delete.php
@@ -0,0 +1,12 @@
+<?php
+include('conn.php');
+
+$id=$_GET['id'];
+$delete=mysqli_query($conn , "DELETE FROM admin where id='$id'");
+
+if ($delete) {
+    # code...
+    header('location:select.php');
+}
+
+?>
--- ADMIN PANNEL/insert.php
+++ ADMIN PANNEL/insert.php
@@ -4,8 +4,8 @@ include('conn.php');
 if (isset($_POST['insert'])) {
     # code...
     $admin_name=$_POST['admin_name'];
-    $admin_passwor=$_POST['admin_password'];
-    $insert=mysqli_query($conn , "INSERT INTO admin VALUES('', '$admin_name', '$admin_passwor') ");
+    $admin_password=$_POST['admin_password'];
+    $insert=mysqli_query($conn , "INSERT INTO admin VALUES('', '$admin_name', '$admin_password') ");
     if ($insert) {
         # code...
         echo "Record inserted";
@@ -24,8 +24,8 @@ if (isset($_POST['insert'])) {
 </head>
 <body>
    <form action="" method="post">
-    Admin Name:<input type="text" name="admin_name">
-    Admin Password:<input type="text" name="admin_passwor">
+    Admin Name:<input type="text" name="admin_name"><br>
+    Admin Password:<input type="text" name="admin_password"> <br>
    <button name="insert">Create Admin</button>
    </form> 
 </body>
