d6929e09da5cdece643e675697af11910a55b957
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-09-27 14:49:10 +0200

    php

--- ADMIN PANNEL/select.php
+++ ADMIN PANNEL/select.php
@@ -14,18 +14,21 @@
     <th colspan="2">Operations</th>
 </tr>
 <?php
-include('conn.php');
-$select =mysqli_query($conn , "SELECT * FROM admin");
-
-while($row=mysqli_fetch_all($select)){
-?>
+        include('conn.php');
+        $select =mysqli_query($conn , "SELECT * FROM admin");
+        while($row=mysqli_fetch_array($select)){
+        ?>
+        
 <tr>
     <td><?php echo $row['id']?></td>
     <td><?php echo $row['admin_name']?></td>
     <td><?php echo $row['admin_password']?></td>
-</tr>
+    <td><a href="delete.php?id=<?php echo $row['id']?>">Delete</a></td>
+    
+
 <?php
-}?>
+        }
+    ?>
    </table> 
 </body>
 </html>
