139e2d0ccce41f290943bab4679605f67c4b8a4e
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-09-27 14:55:37 +0200

    php

--- ADMIN PANNEL/update.php
+++ ADMIN PANNEL/update.php
@@ -18,7 +18,25 @@ $row=mysqli_fetch_all($select);
 <form action="" method="post">
     Admin Name:<input type="text" name="admin_name" value="<?php echo $row['id']?>"><br>
     Admin Password:<input type="text" name="admin_password" value="<?php echo $row['admin_name']?>"> <br>
-   <button name="insert">Create Admin</button>
+   <button name="insert">Update</button>
    </form> 
 </body>
 </html>
+
+
+<?php
+include('conn.php');
+
+if (isset($_POST['insert'])) {
+    # code...
+    $admin_name=$_POST['admin_name'];
+    $admin_password=$_POST['admin_password'];
+    $update=mysqli_query($conn , "UPDATE admin set admin_name='$admin_name', admin_passwor='$admin_password'");
+    if ($update) {
+        # code...
+ header('location:select.php');
+    }
+    else{
+        echo "not really working";
+    }
+}?>
