58f96d961d42fdcd6ea7779955d930d750fb90ee
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-09-28 16:47:28 +0200

    php

---
+++ GRADE/insert.php
@@ -0,0 +1,36 @@
+<?php
+include('conn.php');
+
+if (isset($_POST['insert'])) {
+    # code...
+    $CandidateNationalId=$_POST['CandidateNationalId'];
+    $LicenseExamCategory=$_POST['LicenseExamCategory'];
+    $ObtainedMarks=$_POST['ObtainedMarks'];
+    $Decision=$_POST['Decision'];
+    $insert=mysqli_query($conn , "INSERT INTO admin VALUES('$CandidateNationalId', '$LicenseExamCategory', '$ObtainedMarks' , '$Decision') ");
+    if ($insert) {
+        # code...
+        echo "Record inserted";
+    }
+    else{
+        echo "not really working";
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <form action="" method="post">
+   CandidateNationalId:<input type="text" name="CandidateNationalId"><br>
+   LicenseExamCategory:<input type="text" name=" LicenseExamCategory"> <br>
+   ObtainedMarks:<input type="text" name=" ObtainedMarks/20"> <br>
+   Decision:<input type="text" name="Decision"> <br>
+   <button name="insert">Create </button>
+   </form> 
+</body>
+</html>
